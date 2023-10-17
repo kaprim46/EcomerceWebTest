33f816c83b37a6509b00506d60fa2e74ff84d389
marouanekrizi <kaprim41@outlook.com>
2023-10-17 17:50:10 +0100

    Add all website

---
+++ EcommerceWebTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EcommerceWebTest.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
